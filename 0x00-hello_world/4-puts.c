a24e8d53b3ea34521139032528ed556f6fcf484a
Bridgit <bridgitchepkwony3@gmail.com>
2023-04-20 16:18:33 +0300

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*My first c programming*/
+/**
+ * main - Entry point
+ *
+ * Return: 0 always success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
