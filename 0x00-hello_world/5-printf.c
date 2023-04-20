6717e285deb391106c165bba72e3d94ce1653cf2
Bridgit <bridgitchepkwony3@gmail.com>
2023-04-20 16:46:50 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints a sentence using the printf command
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
