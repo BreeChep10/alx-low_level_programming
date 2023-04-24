a09519cd7267ae3a84506993b545eb4e6648fcf6
Bridgit <bridgitchepkwony3@gmail>
2023-04-24 11:18:31 -0700

    all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,14 +16,18 @@ int main(void)
 		{
 			if (num1 <  num2)
 			{
-				if (!(num1 == 0 && num2 == 1))
-					putchar(',');
+				putchar(',');
 				putchar(' ');
 				putchar(num1 / 10 + 48);
 				putchar(num1 % 10 + 48);
 				putchar(' ');
 				putchar(num2 / 10 + 48);
 				putchar(num2 % 10 + 48);
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
