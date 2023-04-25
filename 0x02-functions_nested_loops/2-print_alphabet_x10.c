45009b874fdb3001c53f1a8b845d1a91d6ed39fe
Bridgit <bridgitchepkwony3@gmail>
2023-04-25 03:57:40 -0700

    Alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - Printing the alphabet 10 times.
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char abc;
+	int x = 0;
+
+	while (x < 10)
+	{
+		for (abc = 'a'; abc <= 'z'; abc++)
+		{
+			_putchar(abc);
+		}
+		_putchar('\n');
+		x++;
+	}
+}
