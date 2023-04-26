18eee2ed84b507bd19e1dabe0e781de99862f892
Bridgit <bridgitchepkwony3@gmail>
2023-04-26 03:37:47 -0700

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
-	else
+	else if (b > a && c > a)
 	{
 		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
 	}
 
 	return (largest);
