607337acdf86687ae85481d75ea59b61b34dde68
Bridgit <bridgitchepkwony3@gmail>
2023-05-04 05:24:56 -0700

    Coverting the string to uppercase.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * string_toupper - Function that converts a string to uppercase.
+ * @str: string to be modified.
+ *
+ * Return: A pointer to the changed string.
+ */
+
+char *string_toupper(char *s)
+{
+	int index = 0;
+
+	while (s[index] != '\0')
+	{
+		index++;
+
+		if (s[index] >= 'a' && s[index] <= 'z')
+		{
+			s[index] -= 32;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	return (s);
+}
