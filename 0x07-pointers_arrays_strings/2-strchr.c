ab37db63a1bfc5f5ac16eefa7d7ae1bb2da28446
Bridgit <bridgitchepkwony3@gmail>
2023-05-08 02:53:23 -0700

    function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,12 +13,11 @@ char *_strchr(char *s, char c)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0'; index++)
+	for (index = 0; s[index] >= '\0'; index++)
 	{
 		if (s[index] == c)
-		{
 			return (s + index);
-		}
 	}
-	return ("\n");
+
+	return ('\0');
 }
