f4c5b26ab417b7b08e7446fe7a714535d6ce79af
Bridgit <bridgitchepkwony3@gmail>
2023-05-12 13:48:14 -0700

    coin count

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,18 +12,18 @@ int coin_count(int cents)
 {
 	if (cents <= 0)
 	{
-		return (0);
+		printf("0\n");
 	}
 	if (cents >= 25)
 	{
 		return (1 + coin_count(cents - 25));
-        }
+	}
 	else if (cents >= 10)
 	{
 		return (1 + coin_count(cents - 10));
 	}
 	else if (cents >= 5)
-        {
+	{
 		return (1 + coin_count(cents - 5));
 	}
 	else if (cents >= 2)
@@ -44,15 +44,15 @@ int coin_count(int cents)
 int main(int argc, char **argv)
 {
 	int cents;
-	
+
 	if (argc - 1 != 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
+
 	cents = atoi(argv[1]);
-	
+
 	printf("%d\n", coin_count(cents));
 
 	return (0);
