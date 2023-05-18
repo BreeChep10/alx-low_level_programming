d987873c474f9d2d70ea229652debf397e483963
Bridgit <bridgitchepkwony3@gmail>
2023-05-18 12:59:13 -0700

    Correct

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,151 +1,97 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-int product(int num1, int num2);
-int is_digit(const char *str);
-int _atoi(const char *str);
-void print_result(int result);
-void input_check(int argc, char *argv[], int *num1, int *num2);
 int main(int argc, char *argv[]);
+void multiply(char *num1, char *num2);
+void print_error(void);
 
 /**
- * product - Calculate the product of two numbers.
- * @num1: First integer in the product.
- * @num2: Second integer in the product.
+ * print_error - Print the word error.
+ */
+
+void print_error(void)
+{
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(98);
+}
+
+/**
+ * multiply - Function that multiplies 2 numbers.
+ * @num1: The first number to multiply.
+ * @num2: The second number to multiply.
  *
  * Return: The product of the two numbers.
  */
-
-int product(int num1, int num2)
+void multiply(char *num1, char *num2)
 {
-	return (num1 * num2);
-}
+	int len1 = 0, len2 = 0, i, j, carry, sum;
+	int *result;
 
-/**
- * is_digit - Checks whether a number is positive or not.
- * @str: Pointer to the string to be checked.
- *
- * Return: The string.
- */
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
 
-int is_digit(const char *str)
-{
-	while (*str != '\0')
+	result = malloc((len1 + len2) * sizeof(int));
+	if (result == NULL)
+		print_error();
+
+	for (i = 0; i < len1 + len2; i++)
+		result[i] = 0;
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		if (*str < '0' || *str > '9')
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			return (0);
+			sum = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+			carry = sum / 10;
+			result[i + j + 1] = sum % 10;
 		}
-		str++;
+		result[i] += carry;
 	}
-	return (1);
+	i = 0;
+
+	while (result[i] == 0 && i < len1 + len2 - 1)
+		i++;
+	for (; i < len1 + len2; i++)
+		_putchar(result[i] + '0');
+	_putchar('\n');
 }
 
 /**
- * _atoi - Function that converst a string to integer.
- * @str: The string to be converted.
- * 
- * Return: The converted string.
- */
-
-int _atoi(const char *str)
-{
-	int num = 0;
-
-	while (*str != '\0')
-	{
-		num = num * 10 + (*str - '0');
-		str++;
-	}
-	return (num);
-}
-
-/**
- * print_result - Printing the result integer to the standard output.
- * @result: The integer to be printed out.
+ * main - Function that takes in arguements and multiplies them.
+ * @argc: Number of arguements passed.
+ * @argv: Pointer to the array of arguements.
  *
- * Return: Void.
- */
-
-void print_result(int result)
-{
-	char buffer[12], *buffer_ptr;
-	int num_digits = 0, i;
-	int temp = result;
-
-	if (temp == 0)
-	{
-		num_digits = 1;
-	}
-	else
-	{
-		while (temp != 0)
-		{
-			temp /= 10;
-			num_digits++;
-		}
-	}
-	buffer_ptr = buffer + num_digits;
-	*buffer_ptr = '\n';
-	buffer_ptr--;
-
-	while (result != 0)
-	{
-		*buffer_ptr = '0' + (result % 10);
-		result /= 10;
-		buffer_ptr--;
-	}
-	for (i = 0; i < num_digits + 1; i++)
-	{
-		_putchar(buffer[i]);
-	}
-}
-/**
- * input_check - Checking the integers entered.
- * @argc: The number of arguements entered.
- * @argv: The arguements entered.
- * @num1; The first number.
- * @num2: The second number.
- *
- * Return: Void
- */
-
-void input_check(int argc, char *argv[], int *num1, int *num2)
-{
-	int i = 0;
-
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
-	{
-		char error[] = "Error\n";
-
-		while (error[i] != '\0')
-		{
-			_putchar(error[i]);
-			i++;
-		}
-		_putchar('\n');
-		exit(98);
-	}
-	*num1 = _atoi(argv[1]);
-	*num2 = _atoi(argv[2]);
-}
-
-/**
- * main - Entry point.
- * @argc: The number of arguements entered.
- * @argv: The arguements entered.
- *
- * Return: The product.
+ * Return: Results.
  */
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, results;
+	int i;
+	char *num1 = argv[1];
+	char *num2 = argv[2];
 
-	input_check(argc, argv, &num1, &num2);
+	if (argc != 3)
+		print_error();
 
-	results = product(num1, num2);
-	print_result(results);
+	for (i = 0; num1[i]; i++)
+	{
+		if (num1[i] < '0' || num1[i] > '9')
+			print_error();
+	}
+	for (i = 0; num2[i]; i++)
+	{
+		if (num2[i] < '0' || num2[i] > '9')
+			print_error();
+	}
+	multiply(num1, num2);
 
 	return (0);
 }
+
