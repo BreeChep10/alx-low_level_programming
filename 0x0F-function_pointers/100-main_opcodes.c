893d1525d569056ecfd2382c3751cc7b194f8b59
Bridgit <bridgitchepkwony3@gmail.com>
2023-05-24 19:38:43 +0300

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,15 +12,16 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int num_bytes;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
-	int num_bytes = atoi(argv[1]);
-	
+	num_bytes = atoi(argv[1]);
+
 	if (num_bytes <= 0)
 	{
 		printf("Error\n");
@@ -28,6 +29,7 @@ int main(int argc, char *argv[])
 	}
 
 	unsigned char *ptr = (unsigned char *)main;
+
 	for (i = 0; i < num_bytes; i++)
 	{
 		printf("%02x ", ptr[i]);
