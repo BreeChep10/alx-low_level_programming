5f12da81cb6f29760a8eb5eb033ddfde8d6dd5c0
Bridgit <bridgitchepkwony3@gmail>
2023-05-25 11:04:39 -0700

    TASK 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,113 +1,54 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
-
-int print_character(va_list args);
-int print_integerN(va_list args);
-int print_string(va_list args);
-int print_floats(va_list args);
 
 /**
  * print_all - Function that prints all.
- * @format: Coontains the list of aarguments.
+ * @format: List all arguements.
  *
  * Return: void.
  */
 
 void print_all(const char * const format, ...)
 {
-	int a, b;
-	char *separator1 = "";
-	char *separator2 = ", ";
+	int a = 0, integerN;
+	char *separator1 = "", *separator2 = ", ", *s, character;
 	va_list all_args;
-	print_t ops[] = {
-		{"c", print_character},
-		{"i", print_integerN},
-		{"s", print_string},
-		{"f", print_floats},
-		{NULL, NULL}
-	};
 
 	va_start(all_args, format);
-	a = 0;
 
-	while (format != NULL && format[a])
+	if (format != NULL)
 	{
-		b = 0;
-
-		while (ops[b].f != NULL)
+		while (format[a])
 		{
-			if (format[a] == *(ops[b].c))
+			switch (format[a])
 			{
-				printf("%s", separator1);
-				ops[b].f(all_args);
+				case 'c':
+					character = va_arg(all_args, int);
+					printf("%s%c", separator1, character);
+					break;
+				case 'i':
+					integerN = va_arg(all_args, int);
+					printf("%s%d", separator1, integerN);
+					break;
+				case 'f':
+					printf("%s%f", separator1, va_arg(all_args, double));
+					break;
+				case 's':
+					s = va_arg(all_args, char *);
+					if (s == NULL)
+						s = "(nil)";
+					printf("%s%s", separator1, s);
+					break;
+				default:
+					a++;
+					continue;
 			}
-			b++;
+			separator1 = separator2;
+			a++;
 		}
-		separator1 = separator2;
-		a++;
 	}
 	printf("\n");
 	va_end(all_args);
-}
-/**
- * print_character - Function that prints character.
- * @args: List of arguements.
- *
- * Return: 0 always.
- */
-
-int print_character(va_list args)
-{
-	printf("%c", va_arg(args, int));
-	return (0);
-}
-
-/**
- * print_integerN - Function that prints an ineteger.
- * @args: Lists of arguements passed.
- *
- * Return: 0 always.
- */
-
-int print_integerN(va_list args)
-{
-	printf("%d", va_arg(args, int));
-	return (0);
-}
-
-/**
- * print_string - Function that prints a string.
- * @args: List of arguements passed.
- *
- * Return: 0 always.
- */
-
-int print_string(va_list args)
-{
-	char *s;
-
-	s = va_arg(args, char *);
-
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return (0);
-	}
-	printf("%s", s);
-	return (0);
-}
-
-/**
- * print_floats - Function that prints float numbers.
- * args: Lists of arguements given.
- *
- * Return: Void
- */
-
-int print_floats(va_list args)
-{
-	printf("%f", va_arg(args, double));
-	return (0);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,16 +6,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#include <stdarg.h>
-/**
- * struct print - struct for 3
- * @c: character to check
- * @f: function to point to
- */
-typedef struct print
-{
-	char *c;
-	int (*f)(va_list args);
-} print_t;
-
 #endif
