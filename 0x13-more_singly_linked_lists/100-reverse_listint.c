f74a25212bfb3b7792ad66e0669c9f443572e656
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-06-05 17:18:14 +0300

    function that reverses a list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Function that prints the list in reverse.
+ * @head: Pointer to the head of the list.
+ *
+ * Return: The updated head address.
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+
+	while (current)
+	{
+		listint_t *next = current->next;
+
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+	return (*head);
+}
