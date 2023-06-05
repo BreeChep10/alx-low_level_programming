f9bde1ed0ad0a8859ffe109adb3f891c77ce3d99
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-06-05 11:25:12 +0300

    Fixing betty

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,10 @@
 #include "lists.h"
 
 /**
+ * listint_len - Function the returns the no, of elemsts in a list.
+ * @h: Pointer to the head of the list.
+ *
+ * Return: The number of elements in a list.
  */
 
 size_t listint_len(const listint_t *h)
