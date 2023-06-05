c5483b5369b900c7597eae9fcb24097a96ae29b3
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-06-05 18:34:50 +0300

    error

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,17 +9,20 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
+	listint_t *ptr;
+	listint_t *next;
 
-	while (current != NULL)
+	ptr = *head;
+	if (!ptr)
+		return (NULL);
+	next = ptr->next;
+	ptr->next = NULL;
+	while (next)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		ptr = next;
+		next = next->next;
+		ptr->next = *head;
+		*head = ptr;
 	}
-	*head = prev;
-	return *head;
+	return (*head);
 }
