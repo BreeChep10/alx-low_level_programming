4f451b5c173460e8b92d9b56922bbe8f787fba8a
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-06-12 19:07:31 +0300

    check

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
