42c6b58056440a0456f69099367281c27a3818ba
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2024-01-17 20:49:36 +0300

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -10,11 +10,13 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t step = sqrt(size);
-	size_t prev = 0, i;
+	size_t step, prev, i;
 
 	if (array == NULL)
 		return (-1);
+
+	step = sqrt(size);
+	prev = 0;
 
 	printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 	while (array[prev] < value && prev < size)
